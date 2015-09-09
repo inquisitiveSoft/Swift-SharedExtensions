1faa0f2253a31dbc6545f53bbc337d717ea47d9b
Harry Jordan <harry@inquisitivesoftware.com>
2015-09-10 00:34:32 +0100

    Added numberOfCharactersInCharacterSet(…) to String.swift

--- Extensions/String.swift
+++ Extensions/String.swift
@@ -36,12 +36,90 @@ extension String {
 		var numberOfWords: UInt = 0
 		let searchRange = self.startIndex..<self.endIndex
 		
-		self.enumerateSubstringsInRange(searchRange, options: NSStringEnumerationOptions.ByWords) { (_, _, _, _) -> () in
+		self.enumerateSubstringsInRange(searchRange, options: [NSStringEnumerationOptions.ByWords, NSStringEnumerationOptions.SubstringNotRequired]) { (_, _, _, _) -> () in
 			numberOfWords++
 		}
 		
 		return numberOfWords
 	}
+	
+	
+
+	func numberOfDecimalCharacters() -> Int {
+		return numberOfCharactersInCharacterSet(NSCharacterSet.decimalDigitCharacterSet())
+	}
+
+	
+	func numberOfCharactersInCharacterSet(characterSet: NSCharacterSet, minimumRun: Int = 0) -> Int {
+		var numberOfMatchingCharacters = 0
+		let scanner = NSScanner(string: self)
+		
+		repeat {
+			scanner.scanUpToCharactersFromSet(characterSet, intoString: nil)
+			
+			var matchingString: NSString? = nil
+			
+			if !scanner.atEnd && scanner.scanCharactersFromSet(characterSet, intoString: &matchingString), let matchingString = matchingString {
+				numberOfMatchingCharacters += matchingString.length
+			}
+		} while (!scanner.atEnd)
+		
+		return numberOfMatchingCharacters
+	}
+	
+	
+	var looksLikeAnIdentifier: Bool {
+		let knownPrefixes = ["text-"]
+		var numberOfIdentifierCharacters = knownPrefixes.filter { self.hasPrefix($0) }.reduce(0) { $0 + $1.characters.count }
+		
+		let identifierCharacterSets = [
+			NSCharacterSet.decimalDigitCharacterSet(),
+			NSCharacterSet(charactersInString: "–-_"),
+			NSCharacterSet.uppercaseLetterCharacterSet()
+		]
+		
+		let combinedIdentifierCharacterSet = NSMutableCharacterSet()
+		
+		for characterSet in identifierCharacterSets {
+			combinedIdentifierCharacterSet.formUnionWithCharacterSet(characterSet)
+		}
+		
+		
+		numberOfIdentifierCharacters += numberOfCharactersInCharacterSet(combinedIdentifierCharacterSet, minimumRun: 5)
+		let stringLength = self.characters.count
+		
+		if (stringLength > 0) && (numberOfIdentifierCharacters > 0) && (Double(numberOfIdentifierCharacters) / Double(stringLength) > 0.55) {
+			return true
+		}
+		
+		return false
+	}
+	
+	
+	func firstLineUpToCharacterCount(desiredNumberOfCharacters: Int) -> String {
+		var numberOfCharacters = 0
+		var desiredRange: Range = startIndex..<endIndex
+		let whitespaceCharacterSet = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+		
+		enumerateLines { (line, stop) -> () in
+			if !line.stringByTrimmingCharactersInSet(whitespaceCharacterSet).isEmpty {
+				line.enumerateSubstringsInRange(line.startIndex..<line.endIndex, options: [NSStringEnumerationOptions.ByWords]) { (substring, _, enclosingRange, stopInternal) -> () in
+					desiredRange = line.startIndex..<enclosingRange.endIndex
+					numberOfCharacters += substring?.characters.count ?? 0
+					
+					if numberOfCharacters >= desiredNumberOfCharacters {
+						stopInternal = true
+					}
+				}
+				
+				stop = true
+			}
+		}
+		
+		let resultingString = substringWithRange(desiredRange).stringByTrimmingCharactersInSet(whitespaceCharacterSet)
+		return resultingString
+	}
+	
 	
 	
 	func substringForUntestedRange(range: NSRange) -> String? {
